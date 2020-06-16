d9b8f8b04eacc76e068f8837620c71c2cbc397b8
Indacluber <dmitrokaryahin@gmail.com>
2020-06-16 21:30:36 +0300

    finish

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener("keydown", (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currenTime = 0;
+  audio.play();
+  key.classList.add("playing");
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+// window.addEventListener("keydown", playSound());
